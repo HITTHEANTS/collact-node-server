ea98b3d65d593b388efee7fc0b67676941487dad
victory-jooyon <gina611.kaist@gmail.com>
2021-03-11 19:28:33 +0900

    Setup user entity

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,16 +1,56 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  JoinColumn,
+  OneToOne,
+  PrimaryGeneratedColumn,
+  Unique,
+  UpdateDateColumn,
+} from 'typeorm';
 
 @Entity()
 export class User {
+  // TODO: set unique columns (nickname&deletedDate, uid, token), implement profile entity
+
   @PrimaryGeneratedColumn()
   id: number;
 
   @Column()
-  firstName: string;
+  nickname: string;
 
-  @Column()
-  lastName: string;
+  @Column({ default: false })
+  isSuperuser: boolean;
+
+  @Column({ default: false })
+  isStaff: boolean;
 
   @Column({ default: true })
   isActive: boolean;
+
+  @Column({ default: 'manual' })
+  loginBy: string;
+
+  @Column({ nullable: true })
+  uid: string;
+
+  @Column({ nullable: true })
+  token: string;
+
+  // @OneToOne(type => Profile, profile => profile.user)
+  // @JoinColumn()
+  // profile: Profile;
+
+  @Column({ default: () => 'CURRENT_TIMESTAMP' })
+  lastLoginDt: Date;
+
+  @CreateDateColumn()
+  createdDate: Date;
+
+  @UpdateDateColumn()
+  updatedDate: Date;
+
+  @DeleteDateColumn()
+  deletedDate: Date;
 }
