9c64cb18a9da23b5637388d585068ba12a2c68e2
victory-jooyon <gina611.kaist@gmail.com>
2021-03-12 20:24:14 +0900

    Generate profile when creating user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
+import { Profile } from '../profiles/profile.entity';
 
 @Injectable()
 export class UsersService {
@@ -25,10 +26,12 @@ export class UsersService {
     }
 
     const user = new User();
+    const profile = new Profile();
     user.nickname = createUserDto.nickname;
     user.loginPlatform = createUserDto.loginPlatform;
     user.uid = createUserDto.uid;
     user.token = createUserDto.token;
+    user.profile = profile;
     return this.usersRepository.save(user);
   }
 
