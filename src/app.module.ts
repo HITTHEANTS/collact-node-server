b07338c675c0cb07263a81afb043c2795abd5feb
SmileJayden <wkdwodud07@gmail.com>
2021-03-24 19:48:39 +0900

    Add logger middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { LoggerMiddleware } from './common/middleware/logger.middleware';
 import ormConfig from '../ormconfig';
 
 @Module({
@@ -19,4 +25,10 @@ import ormConfig from '../ormconfig';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware, LoggerMiddleware)
+      .forRoutes({ path: '*', method: RequestMethod.ALL });
+  }
+}
