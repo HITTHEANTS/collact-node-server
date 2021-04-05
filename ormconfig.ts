ea6132e317d81595282a8c723d67422b61483bef
jaeyoungChang <wkdwodud07@gmail.com>
2021-04-06 01:05:15 +0900

    Fix typeorm configs

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,21 +1,30 @@
+import * as dotenv from 'dotenv';
 import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
+dotenv.config();
+
 const config: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: process.env.DB_HOST,
-  port: +process.env.DB_PORT,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  entities: ['src/**/*.entity.ts'],
-  migrations: ['src/migration/*.ts'],
+  host: process.env.TYPEORM_DB_HOST,
+  port: +process.env.TYPEORM_DB_PORT,
+  username: process.env.TYPEORM_DB_USER,
+  password: process.env.TYPEORM_DB_PASSWORD,
+  database: process.env.TYPEORM_DB_NAME,
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  migrations: [__dirname + '/migration/**/*{.ts,.js}'],
   cli: {
     migrationsDir: 'src/migration',
   },
+  // We are using migrations, synchronize should be set to false.
+  synchronize: false,
+
+  // Run migrations automatically,
+  // you can disable this if you prefer running migration manually.
+  migrationsRun: true,
+
   logging: 'all',
   logger: 'advanced-console',
-  synchronize: true,
   namingStrategy: new SnakeNamingStrategy(),
 };
 
