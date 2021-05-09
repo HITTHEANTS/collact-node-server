5d76283b579bb91c145bef3bbba742ec82b364ee
jaeyoungChang <wkdwodud07@gmail.com>
2021-05-09 11:00:36 +0900

    Fix swagger docu profile patch api body

--- src/profiles/profile.swagger.ts
+++ src/profiles/profile.swagger.ts
@@ -1,5 +1,11 @@
-import { OmitType } from '@nestjs/swagger';
+import { ApiProperty, OmitType, PartialType } from '@nestjs/swagger';
 
+import { UpdateProfileDto } from './dto/update-profile.dto';
 import { Profile } from './profile.entity';
 
 export class ProfileResponse extends OmitType(Profile, ['user'] as const) {}
+
+export class ProfilePatchBody extends PartialType(UpdateProfileDto) {
+  @ApiProperty({ type: 'string', format: 'binary', required: false })
+  file: any | null;
+}
