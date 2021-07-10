ffcae3ee856410450d3b175b3d9e63e5b4231971
SmileJayden <wkdwodud07@gmail.com>
2021-07-10 17:39:11 +0900

    Implement project areas findAll

--- src/projects/projects.swagger.ts
+++ src/projects/projects.swagger.ts
@@ -1,8 +1,13 @@
-import { ApiProperty, PartialType } from '@nestjs/swagger';
+import { ApiProperty, OmitType, PartialType } from '@nestjs/swagger';
 
 import { CreateProjectDto } from './dto/create-project.dto';
+import { ProjectArea } from './project-area.entity';
 
 export class ProjectPostBody extends PartialType(CreateProjectDto) {
   @ApiProperty({ type: 'string', format: 'binary', required: true })
   photos: any[];
 }
+
+export class ProjectsAreaGetResponse extends OmitType(ProjectArea, [
+  'projects',
+] as const) {}
