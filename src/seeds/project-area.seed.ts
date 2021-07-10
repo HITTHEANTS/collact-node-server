bcb1b0cda99e8a98291f89c56bbdaeb615f26b24
victory-jooyon <gina611.kaist@gmail.com>
2021-07-10 17:13:04 +0900

    Add project-area seed data

---
+++ src/seeds/project-area.seed.ts
@@ -0,0 +1,16 @@
+const projectAreas = [
+  'drawing',
+  'design',
+  'photo',
+  'craft',
+  'space',
+  'writing',
+  'video',
+  'music',
+  'fashion',
+  'etc',
+];
+
+export const ProjectAreaSeed = projectAreas.map((area) => ({
+  area,
+}));
